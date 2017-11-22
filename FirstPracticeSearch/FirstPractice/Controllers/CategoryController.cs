7474bb68034287d4292f2137ed76d5f0a7ff4398
JerryChen0585 <33824495+JerryChen0585@users.noreply.github.com>
2017-11-22 22:06:37 +0800

    1stPracticeSearch
    1stPracticeSearch

---
+++ FirstPracticeSearch/FirstPractice/Controllers/CategoryController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstPractice.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace FirstPractice.Controllers
+{
+    public class CategoryController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        private int pageSize = 5;
+        // GET: Category
+        public ActionResult Index(int page=1)
+        {
+          
+            //跟Model要資料
+            List<Category> categories = CategoryDataContext.LoadCategories();
+            int currentPage = page < 1 ? 1 : page;
+            var category = db.Categories.OrderBy(x => x.CategoryID);//資料排序並挑出所有欄位資料
+            var result = categories.ToPagedList(currentPage, pageSize);
+            //將資料傳給View
+            return View(result);
+        }
+        [HttpPost]
+        public ActionResult Index(string id,int page=1)
+        {
+            int currentPage = page < 1 ? 1 : page;
+            var category = db.Categories.OrderBy(x => x.CategoryID);//資料排序並挑出所有欄位資料
+            var category1 = category.Where(xx => xx.CategoryName.Contains(id));//搜尋排序過的資料
+            var result = category1.ToPagedList(currentPage, pageSize);
+            return View(result);
+        }
+        public ActionResult Insert()
+        {
+            if(Request.Form.Count>0)
+            {
+                //接收表單傳過來的資料
+                Category _category = new Category();
+                _category.CategoryName = Request.Form["CategoryName"];
+                _category.Description = Request.Form["Description"];
+                //將資料傳給Model
+                CategoryDataContext.InsertCategory(_category);
+                //轉到另外一個Action
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Delete(int id=0)
+        {
+            CategoryDataContext.DeleteCategory(id);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id=0)
+        {
+            Category _category = CategoryDataContext.LoadCategoryByID(id);
+            return View(_category);
+        }
+        [HttpPost]
+        public ActionResult Edit()
+        {
+            Category _category = new Category();
+            _category.CategoryID = Convert.ToInt32(Request.Form["CategoryID"]);
+            _category.CategoryName = Request.Form["CategoryName"];
+            _category.Description = Request.Form["Description"];
+            CategoryDataContext.EditCategory(_category);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FirstPracticeSearch/FirstPractice/Models/CategoryDataContext.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FirstPractice.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CategoryDataContext
+    {
+        static string strConn = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        //讀取所有產品分類資料
+        public static List<Category> LoadCategories()
+        {
+            List<Category> categories = new List<Category>();
+
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                String strCmd = "select CategoryID,CategoryName,Description from Categories";
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        Category _category = new Category();
+                        _category.CategoryID = Convert.ToInt32(dr["CategoryID"]);
+                        _category.CategoryName = dr["CategoryName"].ToString();
+                        _category.Description = dr["Description"].ToString();
+                        categories.Add(_category);
+                    }
+                    dr.Close();
+                    conn.Close();
+                }
+
+            }
+            return categories;
+        }
+        public static void InsertCategory(Category category)
+        {
+            string strCmd = "insert Categories(CategoryName,Description)values(@CategoryName,@Description)";
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+                    cmd.Parameters.AddWithValue("@Description", category.Description);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        public static void DeleteCategory(int CategoryID)
+        {
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                String strCmd = "Delete Categories where CategoryID=@CategoryID";
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryID", CategoryID);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+        //根據分類編號讀取要修改的產品分類名稱
+        public static Category LoadCategoryByID(int CategoryID)
+        {
+            Category _category = new Category();
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                String strCmd = "select CategoryID,CategoryName,Description from Categories where CategoryID=@CategoryID";
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryID", CategoryID);
+                    conn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if(dr.Read())
+                    {
+                        _category.CategoryID = Convert.ToInt32(dr["CategoryID"]);
+                        _category.CategoryName = dr["CategoryName"].ToString();
+                        _category.Description = dr["Description"].ToString();
+                    }
+                    dr.Close();
+                    conn.Close();
+                }
+            }
+            return _category;
+        }
+        //修改產品分類資料
+        public static void EditCategory(Category category)
+        {
+            string strCmd = "update Categories set CategoryName=@CategoryName,Description=@Description where CategoryID=@CategoryID";
+            using (SqlConnection conn = new SqlConnection(strConn))
+            {
+                using (SqlCommand cmd = new SqlCommand(strCmd, conn))
+                {
+                    cmd.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+                    cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+                    cmd.Parameters.AddWithValue("@Description", category.Description);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
